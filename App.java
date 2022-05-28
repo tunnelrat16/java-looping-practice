993a17cf6f307cb90371636cb18ec77460fcb8c1
tunnelrat16 <tjandmatt@hotmail.com>
2022-05-27 17:37:53 -0700

    initial commit

--- App.java
+++ App.java
@@ -5,10 +5,23 @@ class App {
     App.doWhileLoop();
   }
 
-  private static void forLoop() {
+  private static void forLoop(){
+    for(int i=0; i<5; i++)
+    System.out.println("I love Java");
   }
   private static void whileLoop() {
+    int count = 0;
+    while (count < 5) { 
+    System.out.println("I love Jawas");
+    count++;
+    }
   }
   private static void doWhileLoop() {
+    int count = 10;
+    do {
+      System.out.println(count);
+      count--;
+    } while (count > 0);
+    System.out.println("Launch!");
   }
 }
